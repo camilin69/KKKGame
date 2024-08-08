154bbddca1c8e322f902b1a27637d409a94a3ec4
camilin69 <camilo.merchan02@uptc.edu.co>
2024-08-07 20:16:28 -0400

    pom shit

---
+++ demo1/src/main/java/co/edu/uptc/core/WindowManager.java
@@ -0,0 +1,8 @@
+package co.edu.uptc.core;
+
+public class WindowManager {
+    private static float FOV = (float) Math.toRadians(60);
+    private static float Z_NEAR = 0.1f;
+    private static float Z_FAR = 100.0f;
+
+}
---
+++ demo1/src/main/java/co/edu/uptc/main/Run.java
@@ -0,0 +1,11 @@
+package co.edu.uptc.main;
+
+
+import org.lwjgl.Version;
+
+public class Run {
+    public static void main(String[] args) {
+        System.out.print(Version.getVersion());
+
+    }
+}
